d46525ef05573190f478124b08e2aecbef54cbd9
mnv <mnvx@yandex.ru>
2016-04-03 15:58:51 +0300

    tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -4,15 +4,31 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class ProductRESTControllerTest extends WebTestCase
 {
-    public function testIndex()
+    
+    public function testGet()
     {
         $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
+        $crawler = $client->request('GET', '/api/products/1');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    public function testCGet()
+    {
+//        // create entity manager mock
+//        $entityManagerMock = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+//            ->setMethods(array('persist', 'flush'))
+//            ->disableOriginalConstructor()
+//            ->getMock();
+
+       
+        // next you need inject your mocked em into client's service container
+        $client = static::createClient();
+//        $client->getContainer()->set('doctrine.orm.default_entity_manager', $entityManagerMock);
+
+        $crawler = $client->request('GET', '/api/products/1');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
 }
